63879cc41e142666082291cea3750f485b03e2fd
杨泽 <398893925@qq.com>
2016-07-16 18:46:16 +0800

    Site updated: 2016-07-16 18:46:15

--- js/photo.js
+++ js/photo.js
@@ -4,35 +4,34 @@ function() {
         page: 1,
         offset: 20,
         init: function() {
-            var t = this;
+            var o = this;
             $.getJSON("/photo/output.json",
             function(e) {
-                t.render(t.page, e),
-                t.scroll(e)
+                o.render(o.page, e),
+                o.scroll(e)
             })
         },
-        render: function(t, e) {
-            var o = (t - 1) * this.offset,
-            n = t * this.offset;
-            if (! (o >= e.length)) {
-                for (var a = "",
-                i = o; i < n && i < e.length; i++) a += '<li><div class="img-box">
-				<a class="img-bg" rel="example_group"
-					href="http://o6y0q0v31.bkt.clouddn.com/' + e[i] + '"></a>
-				<img lazy-src="http://o6y0q0v31.bkt.clouddn.com/' + e[i] + '?imageView2/1/w/200/h/200/interlace/1" /></li>';
-                $(".img-box-ul").append(a),
+        render: function(o, e) {
+            var t = (o - 1) * this.offset,
+            i = o * this.offset;
+            if (! (t >= e.length)) {
+                for (var n = "",r = t; i > r && r < e.length; r++)
+					n += '<li><div class="img-box">
+				<a class="img-bg" rel="example_group" href="http://o6y0q0v31.bkt.clouddn.com/' + e[r] + '"></a>
+				<img lazy-src="http://o6y0q0v31.bkt.clouddn.com/' + e[r] + '?imageView2/1/w/200/h/200/interlace/1" /></li>';
+                $(".img-box-ul").append(n),
                 $(".img-box-ul").lazyload(),
                 $("a[rel=example_group]").fancybox()
             }
         },
-        scroll: function(t) {
+        scroll: function(o) {
             var e = this;
             $(window).scroll(function() {
-                var o = window.pageYOffset,
-                n = o + window.innerHeight,
-                a = 0,
-                i = $(".instagram").offset().top + $(".instagram").height();
-                i >= o && i < n + a && e.render(++e.page, t)
+                var t = window.pageYOffset,
+                i = t + window.innerHeight,
+                n = 0,
+                r = $(".photos").offset().top + $(".photos").height();
+                r >= t && i + n > r && e.render(++e.page, o)
             })
         }
     }
