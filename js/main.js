af670dbaa3a357c15b6fde2d01457e65668b4d08
杨泽 <398893925@qq.com>
2016-07-18 10:18:42 +0800

    Site updated: 2016-07-18 10:18:41

--- js/main.js
+++ js/main.js
@@ -168,7 +168,7 @@ require([], function (){
     }
 	
 	if($(".instagram").length) {
-    require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js','/js/instagram.js'], function(obj) {
+    require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
         obj.init();
     });
 	}
